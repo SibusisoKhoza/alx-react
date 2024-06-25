3557f8079a342fc344d8a9ed13d8fbe7c8fccc47
Sibusiso Khoza <sbusiso.khoza5@gmail.com>
2024-06-25 04:21:29 +0200

    Add

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,53 +1,60 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js',
+    header: {
+      import: './modules/header/header.js',
+      dependOn: 'shared',
+    },
+    body: {
+      import: './modules/body/body.js',
+      dependOn: 'shared',
+    },
+    footer: {
+      import: './modules/footer/footer.js',
+      dependOn: 'shared',
+    },
+    shared: 'jquery',
   },
   output: {
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].js',
-    chunkFilename: '[id].[chunkhash].js',
+  },
+  devServer: {
+    contentBase: './public',
+    port: 8564,
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, './public'),
+    open: true,
+    port: 8564,
   },
   performance: {
     maxAssetSize: 1000000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Add other loaders for images, fonts, etc.
+    ],
   },
   optimization: {
     splitChunks: {
       chunks: 'all',
     },
-  },
-  devServer: {
-    contentBase: path.join(__dirname, './public'),
-    compress: true,
-    port: 8564,
-  },
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({filename: 'index.html'})],
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      },
-    ],
   },
 };
